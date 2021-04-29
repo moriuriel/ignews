1b6f60b1ee134786e1e9d9d70983bd20be10d7db
Uriel Mori Vanso <moriuriel@gmail.com>
2021-04-29 11:58:02 -0300

    feature: add faunadb

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,25 @@
-import styles from './styles.module.scss';
+import { signIn, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 interface ISubscribeProps {
   priceId: string;
 }
-export function SubscribeButton({priceId}: ISubscribeProps) {
+export function SubscribeButton({ priceId }: ISubscribeProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
-  )
+  );
 }
